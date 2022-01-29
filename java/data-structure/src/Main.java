75957df76dc60fe02ed75ad00df69e14156327ee
zhuxianzhong <22525482@qq.com>
2022-01-29 22:59:26 +0800

    backtracking

--- java/data-structure/src/Main.java
+++ java/data-structure/src/Main.java
@@ -6,22 +6,30 @@ import java.util.Map;
 public class Main {
 
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-        map.containsKey(5);
-
-        System.out.println("Hello World!");
-        MergeSort ms = new MergeSort();
-        int[] nums = new int[]{2, 3, 29, 6, 15, 37, 555, 76, 79};
-        ms.sort(nums);
-        for (int i = 0; i < nums.length; i++) {
-            System.out.println(nums[i]);
-        }
-
-        String[] letterMap = new String[]{ "df",};
-        String s = "sfsf";
-        s.isEmpty();
-
-        List<Integer> res = new ArrayList<Integer>();
-        res.remove()
+//        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+//        map.containsKey(5);
+//
+//        System.out.println("Hello World!");
+//        MergeSort ms = new MergeSort();
+//        int[] nums = new int[]{2, 3, 29, 6, 15, 37, 555, 76, 79};
+//        ms.sort(nums);
+//        for (int i = 0; i < nums.length; i++) {
+//            System.out.println(nums[i]);
+//        }
+//
+//        String[] letterMap = new String[]{ "df",};
+//        String s = "sfsf";
+//        s.isEmpty();
+//
+//        List<Integer> res = new ArrayList<Integer>();
+//        res.remove(1);
+//
+//        int[][] d = new int[][]{{0, 1}, {1, 0}};
+        char c = 'c';
+        byte b = 125;
+        long l = 234;
+        int i = (int)l;
+        System.out.println(l);
+        System.out.println(i);
     }
 }
