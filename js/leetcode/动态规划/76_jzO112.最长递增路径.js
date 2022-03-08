f87c4516188d67f9f087a5b8df2ab00c0739cc68
unknown <22525482@qq.com>
2022-03-08 10:58:40 +0800

    jzO 112.最长递增路径 递归/记忆化搜索

---
+++ js/leetcode/动态规划/76_jzO112.最长递增路径.js
@@ -0,0 +1,89 @@
+// 剑指 Offer II 112. 最长递增路径
+// 给定一个 m x n 整数矩阵 matrix ，找出其中 最长递增路径 的长度。
+
+// 对于每个单元格，你可以往上，下，左，右四个方向移动。 不能 在 对角线 方向上移动或移动到 边界外（即不允许环绕）。
+
+// 示例 1：
+// 输入：matrix = 
+// [
+//  [9, 9, 4],
+//  [6, 6, 8],
+//  [2, 1, 1]
+// ]
+// 输出：4 
+// 解释：最长递增路径为 [1, 2, 6, 9]。
+
+// 递归
+function longestIncreasingPath(matrix) {
+  let m = matrix.length;
+  let n = matrix[0].length;
+  let dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+  // 递归函数定义：以i，j为起点的最长递增路径
+  function _longestIncreasingPath(matrix, i, j) {
+    if (i < 0 || i >= m || j < 0 || j >= n) {
+      return 0;
+    }
+
+    let max = 1;
+    for (let k = 0; k < dir.length; k++) {
+      let i1 = i + dir[k][0];
+      let j1 = j + dir[k][1];
+      if (i1 >= 0 && i1 < m && j1 >= 0 && j1 < n && matrix[i1][j1] > matrix[i][j]) {
+        max = Math.max(max, 1 + _longestIncreasingPath(matrix, i1, j1));
+      }
+    }
+    
+    return max;
+  }
+
+  let max = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      max = Math.max(max, _longestIncreasingPath(matrix, i, j));
+    }
+  }
+
+  return max;
+}
+
+// 记忆化搜索
+function longestIncreasingPath(matrix) {
+  let m = matrix.length;
+  let n = matrix[0].length;
+  let dir = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+  let memo = Array(m);
+  for (let i = 0; i < m; i++) {
+    memo[i] = Array(n);
+  }
+
+  // 递归函数定义：以i，j为起点的最长递增路径
+  function _longestIncreasingPath(matrix, i, j) {
+    if (i < 0 || i >= m || j < 0 || j >= n) {
+      return 0;
+    }
+    if (memo[i][j] !== undefined) {
+      return memo[i][j];
+    }
+
+    let max = 1;
+    for (let k = 0; k < dir.length; k++) {
+      let i1 = i + dir[k][0];
+      let j1 = j + dir[k][1];
+      if (i1 >= 0 && i1 < m && j1 >= 0 && j1 < n && matrix[i1][j1] > matrix[i][j]) {
+        max = Math.max(max, 1 + _longestIncreasingPath(matrix, i1, j1));
+      }
+    }
+    
+    return memo[i][j] = max;
+  }
+
+  let max = 0;
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      max = Math.max(max, _longestIncreasingPath(matrix, i, j));
+    }
+  }
+
+  return max;
+}
