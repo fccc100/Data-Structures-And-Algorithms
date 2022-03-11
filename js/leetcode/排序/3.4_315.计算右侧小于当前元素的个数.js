f7ca549282fb2d7b75e5bff86227f8c2786d82b4
zhuxianzhong <22525482@qq.com>
2022-03-11 23:26:12 +0800

    315.计算右侧小于当前元素的个数

--- js/leetcode/排序/3.4_315.计算右侧小于当前元素的个数.js
+++ js/leetcode/排序/3.4_315.计算右侧小于当前元素的个数.js
@@ -11,5 +11,60 @@
 // 1 的右侧有 0 个更小的元素
 
 function countSmaller(nums) {
+  let n = nums.length;
+  let res = Array(n).fill(0);
 
+  let temp = Array(n);
+  let indexs = Array(n);
+  let tempIndex = Array(n);
+  for (let i = 0; i < n; i++) {
+    indexs[i] = i;
+  }
+
+  function mergeSort(nums, l, r) {
+    if (l >= r) return;
+    let mid = Math.floor(l + (r - l) / 2);
+    mergeSort(nums, l, mid);
+    mergeSort(nums, mid + 1, r);
+
+    if (nums[mid] > nums[mid + 1]) {
+      merge(nums, l, mid, r);
+    }
+  }
+  function merge(nums, l, mid, r) {
+    for (let i = l; i <= r; i++) {
+      temp[i] = nums[i];
+    }
+
+    let i = l;
+    let j = mid + 1;
+    for (let k = l; k <= r; k++) {
+      if (i > mid) {
+        nums[k] = temp[j];
+        tempIndex[k] = indexs[j];
+        j++;
+      } else if (j > r) {
+        nums[k] = temp[i];
+        res[indexs[i]] += j - mid - 1;
+        tempIndex[k] = indexs[i];
+        i++;
+      } else if (temp[i] <= temp[j]) {
+        nums[k] = temp[i];
+        res[indexs[i]] += j - mid - 1;
+        tempIndex[k] = indexs[i];
+        i++;
+      } else {
+        nums[k] = temp[j];
+        tempIndex[k] = indexs[j];
+        j++;
+      }
+    }
+
+    for (let i = l; i <= r; i++) {
+      indexs[i] = tempIndex[i];
+    }
+  }
+
+  mergeSort(nums, 0, nums.length - 1);
+  return res;
 }
