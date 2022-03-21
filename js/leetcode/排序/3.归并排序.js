195a64eec798cbeb82922768aa4ec59595ff6586
zhuxianzhong <22525482@qq.com>
2022-03-21 23:20:00 +0800

    merge sort

--- js/leetcode/排序/3.归并排序.js
+++ js/leetcode/排序/3.归并排序.js
@@ -478,6 +478,42 @@ function merge(nums, l, mid, r) {
   }
 }
 
-function sortArray() {
-  
+// 20220321
+function sortArray(nums) {
+  mergeSort(nums, 0, nums.length - 1);
+  return nums;
+}
+
+function mergeSort(nums, l, r) {
+  if (l >= r) {
+    return;
+  }
+  let mid = l + (r - l >> 1);
+  mergeSort(nums, l, mid);
+  mergeSort(nums, mid + 1, r);
+  if (nums[mid] > nums[mid + 1]) {
+    merge(nums, l, mid, r);
+  }
+}
+
+function merge(nums, l, mid, r) {
+  let temp = nums.slice(l, r + 1);
+
+  let i = l;
+  let j = mid + 1;
+  for (let k = l; k <= r; k++) {
+    if (i > mid) {
+      nums[k] = temp[j - l];
+      j++;
+    } else if (j > r) {
+      nums[k] = temp[i - l];
+      i++;
+    } else if (temp[i - l] < temp[j - l]) {
+      nums[k] = temp[i - l];
+      i++;
+    } else {
+      nums[k] = temp[j - l];
+      j++;
+    }
+  }
 }
