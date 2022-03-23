c8faeabbadcd9cd9b179376d0e2ebc846d2b6be9
zhuxianzhong <22525482@qq.com>
2022-03-23 23:55:08 +0800

    20220323 quick sort

--- js/leetcode/排序/4.快速排序.js
+++ js/leetcode/排序/4.快速排序.js
@@ -106,3 +106,45 @@ function swap(nums, i, j) {
 function randomIndex(l, r) {
   return l + Math.round(Math.random() * (r - l));
 }
+
+//20220323
+function sortArray(nums) {
+  quickSort(nums, 0, nums.length - 1);
+  return nums;
+}
+
+function quickSort(nums, l, r) {
+  if (l >= r) {
+    return;
+  }
+
+  let p = partition(nums, l, r);
+  quickSort(nums, l, p - 1);
+  quickSort(nums, p + 1, r);
+}
+
+function partition(nums, l, r) {
+  let p = randomIndex(l, r);
+  swap(nums, p, l);
+  
+  let j = l;
+  for (let i = l + 1; i <= r; i++) {
+    if (nums[i] < nums[l]) {
+      j++;
+      swap(nums, i, j);
+    }
+  }
+
+  swap(nums, j, l);
+  return j;
+}
+
+function swap(nums, i, j) {
+  let temp = nums[i];
+  nums[i] = nums[j];
+  nums[j] = temp;
+}
+
+function randomIndex(l, r) {
+  return l + Math.round(Math.random() * (r - l));
+}
