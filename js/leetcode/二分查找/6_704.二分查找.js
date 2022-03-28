f4918f9af7372dc6b524d09adaa0df4d16f93915
zhuxianzhong <22525482@qq.com>
2022-03-29 00:05:35 +0800

    binary search

---
+++ js/leetcode/二分查找/6_704.二分查找.js
@@ -0,0 +1,26 @@
+// 704. 二分查找
+// 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target  ，写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+
+
+// 示例 1:
+
+// 输入: nums = [-1,0,3,5,9,12], target = 9
+// 输出: 4
+// 解释: 9 出现在 nums 中并且下标为 4
+
+function search(nums, target) {
+  let l = 0;
+  let r = nums.length - 1;
+  while(l <= r) {
+    let mid = l + (r - l >> 1);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      l = mid + 1;
+    } else {
+      r = mid - 1;
+    }
+  }
+
+  return -1;
+}
