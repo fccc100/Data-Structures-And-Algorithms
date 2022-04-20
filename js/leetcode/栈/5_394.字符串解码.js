c30e926a3ea07be81a29c66a2430674d9c5c4d49
unknown <22525482@qq.com>
2022-04-20 16:12:26 +0800

    用队列实现栈

--- js/leetcode/栈/5_394.字符串解码.js
+++ js/leetcode/栈/5_394.字符串解码.js
@@ -58,50 +58,6 @@ function decodeString(s) {
   // return res;
 }
 
-  /**
-     * 双栈解法：
-     * 准备两个栈，一个存放数字，一个存放字符串
-     * 遍历字符串分4中情况
-     * 一、如果是数字 将字符转成整型数字 注意数字不一定是个位 有可能是十位，百位等 所以digit = digit*10 + ch - '0';
-     * 二、如果是字符 直接将字符放在临时字符串中
-     * 三、如果是"[" 将临时数字和临时字符串入栈
-     * 四、如果是"]" 将数字和字符串出栈 此时临时字符串res = 出栈字符串 + 出栈数字*res
-     */
-   public String decodeString(String s) {
-    //创建数字栈，创建字符串栈 及临时数字和临时字符串
-    Deque<Integer> stack_digit = new LinkedList<>();
-    Deque<StringBuilder> stack_string = new LinkedList<>();
-    int digit = 0;
-    StringBuilder res = new StringBuilder();
-    //遍历字符串 分4中情况
-    for (int i = 0; i < s.length(); i++) {
-        char ch = s.charAt(i);
-        if (ch == '[') {
-            //如果是"[" 将临时数字和临时字符串入栈
-            stack_digit.push(digit);
-            stack_string.push(res);
-            digit = 0;
-            res = new StringBuilder();
-        }else if (ch == ']') {
-            //如果是"]" 将数字和字符串出栈 此时临时字符串res = 出栈字符串 + 出栈数字*res
-            StringBuilder temp = stack_string.poll();
-            int count = stack_digit.poll();
-            for (int j = 0; j < count; j++) {
-                temp.append(res.toString());
-            }
-            res = temp;
-        }else if (Character.isDigit(ch)) {
-            //如果是数字 将字符转成整型数字 ch-‘0’。 注意数字不一定是个位 比如100[a] 所以digit要*10
-            digit = digit*10 + ch - '0';
-        }else {
-            //如果是字符 直接将字符放在临时字符串中
-            res.append(ch);
-        }
-    }
-    return res.toString();
-}
-
-
 function decodeString(s) {
   // let numStack = [];
   // let strStack = [];
---
+++ js/leetcode/栈/9_225.用队列实现栈.js
@@ -0,0 +1,91 @@
+// 225. 用队列实现栈
+// 请你仅使用两个队列实现一个后入先出（LIFO）的栈，并支持普通栈的全部四种操作（push、top、pop 和 empty）。
+
+// 实现 MyStack 类：
+
+// void push(int x) 将元素 x 压入栈顶。
+// int pop() 移除并返回栈顶元素。
+// int top() 返回栈顶元素。
+// boolean empty() 如果栈是空的，返回 true ；否则，返回 false 。
+ 
+// 注意：
+
+// 你只能使用队列的基本操作 —— 也就是 push to back、peek/pop from front、size 和 is empty 这些操作。
+// 你所使用的语言也许不支持队列。 你可以使用 list （列表）或者 deque（双端队列）来模拟一个队列 , 只要是标准的队列操作即可。
+ 
+// 示例：
+
+// 输入：
+// ["MyStack", "push", "push", "top", "pop", "empty"]
+// [[], [1], [2], [], [], []]
+// 输出：
+// [null, null, null, 2, 2, false]
+
+// 解释：
+// MyStack myStack = new MyStack();
+// myStack.push(1);
+// myStack.push(2);
+// myStack.top(); // 返回 2
+// myStack.pop(); // 返回 2
+// myStack.empty(); // 返回 False
+
+var MyStack = function() {
+  this.queue1 = [];
+  this.queue2 = [];
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyStack.prototype.push = function(x) {
+  this.queue1.push(x);
+  return null;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.pop = function() {
+  if (!this.queue1.length) return -1;
+  while(this.queue1.length > 1) {
+    this.queue2.push(this.queue1.shift());
+  }
+  let ret = this.queue1.shift();
+  while(this.queue2.length) {
+    this.queue1.push(this.queue2.shift());
+  }
+  return ret;
+};
+
+/**
+ * @return {number}
+ */
+MyStack.prototype.top = function() {
+  if (!this.queue1.length) return null;
+  while(this.queue1.length > 1) {
+    this.queue2.push(this.queue1.shift());
+  }
+  let ret = this.queue1.shift();
+  this.queue2.push(ret);
+  while(this.queue2.length) {
+    this.queue1.push(this.queue2.shift());
+  }
+  return ret;
+};
+
+/**
+ * @return {boolean}
+ */
+MyStack.prototype.empty = function() {
+  return this.queue1.length == 0;
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * var obj = new MyStack()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.empty()
+ */
