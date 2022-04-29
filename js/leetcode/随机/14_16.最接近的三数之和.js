ca9126859719a1c2c9d541ffa5284b479bfcb240
unknown <22525482@qq.com>
2022-04-29 18:10:58 +0800

    最接近的三数之和题干

---
+++ js/leetcode/随机/14_16.最接近的三数之和.js
@@ -0,0 +1,35 @@
+// 16. 最接近的三数之和
+// 给你一个长度为 n 的整数数组 nums 和 一个目标值 target。请你从 nums 中选出三个整数，使它们的和与 target 最接近。
+
+// 返回这三个数的和。
+
+// 假定每组输入只存在恰好一个解。
+
+// 示例 1：
+
+// 输入：nums = [-1,2,1,-4], target = 1
+// 输出：2
+// 解释：与 target 最接近的和是 2 (-1 + 2 + 1 = 2) 。
+// 示例 2：
+
+// 输入：nums = [0,0,0], target = 1
+// 输出：0
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var threeSumClosest = function(nums, target) {
+  if (nums.length < 3) return -1;
+
+  let n = nums.length;
+  let res = 0;
+  let distance = Number.MAX_SAFE_INTEGER;
+  for (let i = 0; i < n; i++) {
+    let l = i + 1;
+    let r = n - 1;
+
+    
+  }
+};
