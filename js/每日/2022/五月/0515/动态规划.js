29e9d7539b62d42e8a2e088e817b14f696013a75
zhuxianzhong <22525482@qq.com>
2022-05-17 00:16:51 +0800

    0515

---
+++ js/每日/2022/五月/0515/动态规划.js
@@ -0,0 +1,24 @@
+// 64. 最小路径和
+var minPathSum = function(grid) {
+  let m = grid.length;
+  if (m == 0) return 0;
+  let n = grid[0].length;
+  let dp = Array(m);
+  dp[0] = Array(n).fill(0);
+  dp[0][0] = grid[0][0];
+
+  for (let i = 1; i < m; i++) {
+    dp[i] = Array(n).fill(0);
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  for (let i = 1; i < n; i++) {
+    dp[0][i] = dp[0][i - 1] + grid[0][i];
+  }
+
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+    }
+  }
+  return dp[m - 1][n - 1];
+};
