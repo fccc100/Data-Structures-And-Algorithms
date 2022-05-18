1bff442c679768ab4a0c79e847525c93d722ad2b
朱贤忠 <22525482@qq.com>
2022-05-18 18:17:57 +0800

    设计哈希映射

---
+++ js/leetcode/随机/25_706.设计哈希映射.js
@@ -0,0 +1,115 @@
+// 706. 设计哈希映射
+// 不使用任何内建的哈希表库设计一个哈希映射（HashMap）。
+
+// 实现 MyHashMap 类：
+
+// MyHashMap() 用空映射初始化对象
+// void put(int key, int value) 向 HashMap 插入一个键值对 (key, value) 。如果 key 已经存在于映射中，则更新其对应的值 value 。
+// int get(int key) 返回特定的 key 所映射的 value ；如果映射中不包含 key 的映射，返回 -1 。
+// void remove(key) 如果映射中存在 key 的映射，则移除 key 和它所对应的 value 。
+ 
+
+// 示例：
+
+// 输入：
+// ["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+// [[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+// 输出：
+// [null, null, null, 1, -1, null, 1, null, -1]
+
+// 解释：
+// MyHashMap myHashMap = new MyHashMap();
+// myHashMap.put(1, 1); // myHashMap 现在为 [[1,1]]
+// myHashMap.put(2, 2); // myHashMap 现在为 [[1,1], [2,2]]
+// myHashMap.get(1);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,2]]
+// myHashMap.get(3);    // 返回 -1（未找到），myHashMap 现在为 [[1,1], [2,2]]
+// myHashMap.put(2, 1); // myHashMap 现在为 [[1,1], [2,1]]（更新已有的值）
+// myHashMap.get(2);    // 返回 1 ，myHashMap 现在为 [[1,1], [2,1]]
+// myHashMap.remove(2); // 删除键为 2 的数据，myHashMap 现在为 [[1,1]]
+// myHashMap.get(2);    // 返回 -1（未找到），myHashMap 现在为 [[1,1]]
+
+class TreeNode {
+  constructor(key, val) {
+    this.key = key;
+    this.val = val;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+
+  add(key, val) {
+    this.root = this.__add(this.root, key, val);
+  }
+
+  __add(node, key, val) {
+    if (node == null) {
+      return new TreeNode(key, val);
+    }
+    if (node.key < key) {
+      node.right = this.__add(node.right, key, val);
+    } else if (node.key > key) {
+      node.left = this.__add(node.left, key, val);
+    }
+    return node;
+  }
+
+  get(key) {
+    return this.__get(this.root, key);
+  }
+
+  __get(node, key) {
+    if (node == null) {
+      return -1;
+    }
+    if (node.key > key) {
+      return this.__get(node.left, key);
+    } else if (node.key < key) {
+      return this.__get(node.right, key);
+    } else {
+      return node.val;
+    }
+  }
+}
+
+var MyHashMap = function() {
+  this.treeMap = new BST();
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+MyHashMap.prototype.put = function(key, value) {
+  this.treeMap.add(key, value);
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+MyHashMap.prototype.get = function(key) {
+  this.treeMap.get(key);
+};
+
+/** 
+ * @param {number} key
+ * @return {void}
+ */
+MyHashMap.prototype.remove = function(key) {
+  
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
